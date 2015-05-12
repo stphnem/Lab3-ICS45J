7dd9eb2ecbda1f6f0426d8cfd2e7935102456557
Stephen Em <stphnem@gmail.com>
2015-05-11 18:00:13 -0700

    Added Lab3 Files

---
+++ Lab3/src/BouncingFrame.java
@@ -0,0 +1,42 @@
+// BouncingFrame.java - build the Smiley frame
+// 
+// ICS 45J : Lab Assignment 3
+// 
+// Completed by: [YOUR NAME]
+// UCInetiD:     [YOUR UCInetID]
+// ID:           [YOUR UCI ID Number]
+// Modified:     [LAST MODIFICATION DATE]
+// 
+// Make the frame consisting of a panel in which smiley faces are drawn,
+// building on the attributes of the BasicFrame
+
+public class BouncingFrame extends BasicFrame
+{
+	
+	// The frame contains an animation of a bouncing smiley group
+	
+	// public inherited constants:
+	// HEIGHT: height of the screen
+	// WIDTH: width of the screen
+	
+	// add needed fields here
+	
+	// Construct a bouncing frame from a basic frame - their characteristics are the same:
+	//	height of HEIGHT, width of WIDTH, not resizeable, titled with the given title,
+	//  program exits when the close box hit
+	
+	public BouncingFrame(String title)
+	{
+		// complete
+	}
+	
+	// activateAnimation: from the given bouncing group and display, construct a 
+	//  smiley animation, make the bouncing frame visible, and fire up the animation
+	
+	public void activateAnimation(BouncingGroup bouncers, BouncingDisplay bounceDisplay)
+	{
+		// complete
+	}
+	
+}
+
