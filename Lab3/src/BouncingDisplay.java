7ee4f4e3e329842e482c5d672fe8291f97bd3b1c
Jenny Tang <jennyct@uci.edu>
2015-05-14 16:54:36 -0700

    Added Documentation

--- Lab3/src/BouncingDisplay.java
+++ Lab3/src/BouncingDisplay.java
@@ -5,20 +5,19 @@
 // Completed by: Jenny Tang and Stephen Em
 // UCInetiD:     jennyct, ems
 // ID:           45502833, 33819914
-// Modified:     
+// Modified:     05/14/2015
 
 import java.awt.*;
 import java.awt.geom.*;
 
 
-// Panel that displays the moving smiley
-
+/*
+ * BouncingDisplay Class
+ * Extends BasicDisplay
+ * Panel that displays the moving smiley
+ */
 class BouncingDisplay extends BasicDisplay
-{
-	
-	// Constants go here. You'll likely want them for the four edges
-	// of the frame, the thickness of the walls, and perhaps others
-	
+{	
 	private final static int THICKNESS = 10;
 	
 	private static Point leftWallPt   = new Point(0, THICKNESS);
@@ -26,21 +25,13 @@ class BouncingDisplay extends BasicDisplay
 	private static Point bottomWallPt = new Point(THICKNESS, BouncingFrame.HEIGHT-THICKNESS*4);
 	private static Point topWallPt    = new Point(THICKNESS, 0);
 	
-	
 	private static Dimension leftWallDim   = new Dimension(THICKNESS, BouncingFrame.HEIGHT-THICKNESS*5);
 	private static Dimension rightWallDim  = new Dimension(THICKNESS, BouncingFrame.HEIGHT-THICKNESS*5);
 	private static Dimension topWallDim    = new Dimension(BouncingFrame.WIDTH-THICKNESS*3, THICKNESS);
 	private static Dimension bottomWallDim = new Dimension(BouncingFrame.WIDTH-THICKNESS*3, THICKNESS);
 	
-	
 	private static Wall leftWall, rightWall, bottomWall, topWall;
-	
-	
-	// Needed fields go here. You'll likely want fields for the moving
-	// smileys, the graphics 2D environment, the x and y coordinates
-	// of the upper left corner of the current face part, and for each
-	// of the four walls -- and perhaps others as well
-	
+		
 	private AnimatedSmiley smiley1;
 	private AnimatedSmiley smiley2;
 	private AnimatedSmiley smiley3;
@@ -49,41 +40,29 @@ class BouncingDisplay extends BasicDisplay
 	
 	private int upperLeftX;
 	private int upperLeftY;
-	
-	
-	// A wall of the display (off which the smiley bounces). The outer
-	// class will make four objects of this class, one for each wall.
-	// Not required (since private); still, strongly recommended!
-	
-	private static class Wall
-	{
-		// A wall consists of a rectangle, color, name, and edge --
-		// the position of the edge of the wall that the smiley touches.
 		
+	/*
+	 * Wall: private static class
+	 * Walls in the display that smileys bounce off of.
+	 */
+	private static class Wall
+	{	
 		private Rectangle wallShape;
 		private Color wallColor;
 		private int wallEdge;
 		
-		
-		// Build a wall, given its name and color -- we can figure out
-		// the wall's shape from the provided name and the geometry of the
-		// screen--and provide that info to Rectangle() to build the wall
-		
-
-		
+		/*
+		 * Class constructor.
+		 * Given the name and color, you can figure out the wall's
+		 * shape from the name. We use Rectangle() to construct the wall
+		 * 
+		 * @param position a wall name that lets us know the wall's position
+		 * @param c        the color of the wall
+		 */
 		public Wall(WallName position, Color c)
 		{
-			// walls have a left position, top position, length in x dimension,
-			// length in y dimension, color the same as c; which wall we're
-			// making is indicated by position (WallName.LEFT, 
-			// WallName.RIGHT, WallName.TOP, WallName.BOTTOM)
 			wallColor = c;
 			
-			// figure out upper left, upper right, xLength and 
-			// yLength for each rectangle representing a wall, 
-			// and the edge the smiley will hit when it touches 
-			// a wall, using information about the display screen 
-			// and frame, and the wall's thickness
 			if (position == WallName.RIGHT) {
 				wallShape = new Rectangle(rightWallPt, rightWallDim);
 				wallEdge = (int) (rightWallPt.getX());
@@ -103,12 +82,15 @@ class BouncingDisplay extends BasicDisplay
 		}
 	}
 	
-	// Inherited method:
-	// 	public void repaint() - forces Java to redraw this display
-	
-	// The bouncing display is a display with
-	// four walls (left, right, top, bottom) and three moving smileys
-	
+	/*
+	 * BouncingDisplay constructor
+	 * A display with four walls and three moving smileys
+	 * The walls consist of a top wall, a bottom wall, a right
+	 * wall, and a left wall
+	 * 
+	 * @param bouncingGroup gets three moving smileys from this group
+	 * 
+	 */
 	public BouncingDisplay(BouncingGroup bouncingGroup) {
 	
 		smiley1 = bouncingGroup.getSmiley1();
@@ -121,23 +103,16 @@ class BouncingDisplay extends BasicDisplay
 		bottomWall = new Wall(WallName.BOTTOM, Color.GREEN);
 	}
 	
-	// paintComponent: called by the runtime environment whenever
-	// it thinks the displayed screen has changed, or as soon as
-	// possible after the program makes a call to repaint()
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see BasicDisplay#paintComponent(java.awt.Graphics)
+	 */
 	public void paintComponent(Graphics g)
-	{
-		
-		// Done as a matter of course as the first two
-		// lines of a paintComponent routine
-		
+	{		
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		
 		graphicsManager = g2;
-		
-		// Draw each smiley onto its place on the screen
-		// The moving smileys are now the previous smileys...
 
 		g2.setColor(leftWall.wallColor);
 		g2.fill(leftWall.wallShape);
@@ -161,8 +136,13 @@ class BouncingDisplay extends BasicDisplay
 		
 	}
 	
-	// Return the wall's edge
-	
+	/*
+	 * Returns an int that resembles the wall's edge
+	 * of the wall provided
+	 * 
+	 * @param wallName the name of the wall to get its edge
+	 * @return int     the coordinate of the wall's edge
+	 */
 	public int getWallEdge(WallName wallName) {
 	
 		if (wallName == WallName.BOTTOM) {
@@ -181,7 +161,13 @@ class BouncingDisplay extends BasicDisplay
 			return 0;
 		}
 	}		
-		
+	
+	/*
+	 * Returns the color of the wall provided
+	 * 
+	 * @param wallName the name of the wall to get its color
+	 * @return Color   the color of the wall provided
+	 */
 	public Color getWallColor(WallName wallName) {
 		if (wallName == WallName.BOTTOM) {
 			return bottomWall.wallColor;
@@ -200,6 +186,13 @@ class BouncingDisplay extends BasicDisplay
 		}
 	}
 	
+	/*
+	 * Returns void but ultimately sets the wall provided
+	 * to the color provided
+	 * 
+	 * @param wallName the name of the wall to set
+	 * @param c        the color to set the wall
+	 */
 	public void setWallColor(WallName wallName, Color c) {
 	
 		if (wallName == WallName.BOTTOM) {
@@ -216,23 +209,23 @@ class BouncingDisplay extends BasicDisplay
 		}
 	}
 	
-	// The methods described below are private, and so
-	// only suggested; however, the functionality they 
-	// provide will almost certainly be needed, regardless of 
-	// whether you choose to implement them.
-	
-	// computeUpperLeft() determines the x- and y-coordinate of the
-	// upper-left of a SmileyFacePart.  This should be called whenever
-	// an attributes change would cause the upper-left position to
-	// change. It's recommended because the graphic grawing routines
-	// need to be given theupper left corner of a figure (in addition
-	// to x and y legnths) to draw it.
-	
+	/*
+	 * Returns void but gets the upperleft x and y coordinates of
+	 * the part provided
+	 * 
+	 * @param part a SmileyFacePart to be computed for coordinates
+	 */
 	private void computeUpperLeft(SmileyFacePart part) {
 		upperLeftX = part.getCenterX() - (int) (part.getXLength()/2);
 		upperLeftY = part.getCenterY() - (int) (part.getYLength()/2);
 	}
 	
+	/*
+	 * Returns void but draws all the SmileyFaceParts from the 
+	 * provided SmileyFace
+	 * 
+	 * @param cntSmiley a SmileyFace to draw all its parts
+	 */
 	private void drawSmiley(SmileyFace cntSmiley) {
 	
 		drawPart(cntSmiley.getFace());
@@ -241,10 +234,12 @@ class BouncingDisplay extends BasicDisplay
 		drawPart(cntSmiley.getSmile());
 	}
 	
-	// drawPart: make an ellipse corresponding to the shape 
-	// of the given smiley face part, and draw it, filled
-	// in with the part's color
-	
+	/*
+	 * Returns void but draws the part provided
+	 * 
+	 * @param part a SmileyFacePart which can be a leftEye,
+	 * rightEye, Face, or Smile
+	 */
 	private void drawPart(SmileyFacePart part) {
 	
 		computeUpperLeft(part);
