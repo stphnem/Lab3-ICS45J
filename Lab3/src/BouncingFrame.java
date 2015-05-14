a3c8b06ee68aa8073df7364cdb941acac3a713d3
Jenny Tang <jennyct@uci.edu>
2015-05-14 16:54:10 -0700

    Added Documentation

--- Lab3/src/BouncingFrame.java
+++ Lab3/src/BouncingFrame.java
@@ -8,26 +8,22 @@ import java.awt.Color;
 // Completed by: Jenny Tang and Stephen Em
 // UCInetiD:     jennyct, ems
 // ID:           45502833, 33819914
-// Modified:     [LAST MODIFICATION DATE]
-// 
-// Make the frame consisting of a panel in which smiley faces are drawn,
-// building on the attributes of the BasicFrame
+// Modified:     05/14/2015
 
+
+/*
+ * BouncingFrame Class
+ * Extends BasicFrame
+ * A frame that consists of a panel in which the smiley faces are drawn
+ */
 public class BouncingFrame extends BasicFrame
 {
-	
-	// The frame contains an animation of a bouncing smiley group
-	
-	// public inherited constants:
-	// HEIGHT: height of the screen
-	// WIDTH: width of the screen
-	
-	// add needed fields here
-	
-	// Construct a bouncing frame from a basic frame - their characteristics are the same:
-	//	height of HEIGHT, width of WIDTH, not resizeable, titled with the given title,
-	//  program exits when the close box hit
-	
+	/*
+	 * BouncingFrame constructor
+	 * Constructs a bouncing frame from a basic frame
+	 * 
+	 * @param title a string for the title of the bouncing frame
+	 */
 	public BouncingFrame(String title)
 	{
 		super(title);
@@ -38,9 +34,13 @@ public class BouncingFrame extends BasicFrame
 		setBackground(Color.black);
 	}
 	
-	// activateAnimation: from the given bouncing group and display, construct a 
-	//  smiley animation, make the bouncing frame visible, and fire up the animation
-	
+	/*
+	 * Returns void but constructs a smiley animation while making the frames visible
+	 * and fires up the animation
+	 * 
+	 * @param bouncers      a BouncingGroup of smileys
+	 * @param bounceDisplay a BouncingDisplay that displays the smileys
+	 */
 	public void activateAnimation(BouncingGroup bouncers, BouncingDisplay bounceDisplay)
 	{
 		SmileyAnimation smileyAnimation = new SmileyAnimation(bouncers, bounceDisplay);
